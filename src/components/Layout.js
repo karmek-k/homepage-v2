f04639b1328892f95c114453fef0b47f0868bbd5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-18 20:17:38 +0100

    Add css for the layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,16 @@ import Navbar from './Navbar';
 import Container from './Container';
 import Footer from './Footer';
 
+import styles from './styles/layout.module.css';
+import './styles/global.css';
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={styles.layout}>
       <Navbar />
       <Container content={children} />
       <Footer />
-    </>
+    </div>
   );
 };
 
