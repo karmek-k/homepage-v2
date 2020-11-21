6a913b5e08a659d4ceb32e990d0e1de37141d40c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-21 18:26:23 +0100

    Add a static query for navbar header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, StaticQuery } from 'gatsby';
 
 import styles from './styles/navbar.module.css';
 
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
 const Navbar = () => {
   return (
-    <nav>
-      <h1 className={styles.navTitle}>Site title</h1>
-      <ul className={styles.navLinks}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/skills">Skills</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <StaticQuery
+      query={query}
+      render={data => (
+        <nav>
+          <h1 className={styles.navTitle}>{data.site.siteMetadata.title}</h1>
+          <ul className={styles.navLinks}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    />
   );
 };
 
