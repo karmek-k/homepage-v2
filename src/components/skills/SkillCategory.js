c8de062c930a383de2459c8dfb2a849ca6a389ca
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-11-23 14:22:57 +0100

    Add some bottom margin to skill categories

--- src/components/skills/SkillCategory.js
+++ src/components/skills/SkillCategory.js
@@ -7,12 +7,12 @@ const SkillCategory = ({ skills, categoryName }) => {
   skills = skills.filter(s => s.category.categoryName === categoryName);
 
   return (
-    <>
+    <section className={styles.skillCategorySection}>
       <h3 className={styles.skillCategoryTitle}>{categoryName}</h3>
       {skills.map(s => (
         <Skill key={s.name} name={s.name} description={s.description} />
       ))}
-    </>
+    </section>
   );
 };
 
